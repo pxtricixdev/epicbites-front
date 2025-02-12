510968bc18dc6946710f1d07dcf2c5f5fb171920
Patricia <patricia.abadaranda@gmail.com>
2025-02-12 20:34:05 +0100

    add navigation component in mobile

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,7 +1,13 @@
+<script setup lang="ts">
+import NavbarMobile from '@/components/NavbarMobile.vue'
+import { navbarLinks } from '@/data/navbarLinks'
+</script>
+
 <template>
   <header><h1>Esto será el header</h1></header>
   <main>
     <RouterView />
   </main>
+  <NavbarMobile :links="navbarLinks" />
   <footer>Esto será el footer</footer>
 </template>
