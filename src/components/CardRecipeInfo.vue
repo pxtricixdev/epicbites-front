1ac3d6938608702653da7314e5293a2fb4245dfd
Patricia <patricia.abadaranda@gmail.com>
2025-02-28 19:04:20 +0100

    add CardRecipeInfo component for displaying recipe details in recipes page

---
+++ src/components/CardRecipeInfo.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="card-recipe">
+    <RouterLink :to="link">
+      <img :src="image" :alt="alt" class="card-recipe__image" />
+    </RouterLink>
+    <button class="card-recipe__button"><Heart :size="24" /></button>
+    <div class="card-recipe__info">
+      <h3 class="card-recipe__title">{{ title }}</h3>
+      <div class="card-recipe__tags">
+        <div class="card-recipe__meal">{{ meal }}</div>
+        <div class="card-recipe__diet">{{ diet }}</div>
+        <div class="card-recipe__flavour">{{ flavour }}</div>
+      </div>
+
+      <div class="card-recipe__details">
+        <div class="card-recipe__time">
+          <Clock color="#676767" :size="15" />
+          {{ time }}'
+        </div>
+        <span
+          class="card-recipe__difficulty"
+          :style="{
+            paddingLeft: '8px',
+            paddingRight: '8px',
+            borderRadius: '3px',
+            backgroundColor:
+              difficulty === 'Facil' ? '#7fe570' : difficulty === 'Media' ? 'orange' : 'red',
+          }"
+          >{{ difficulty }}
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Clock, Heart } from 'lucide-vue-next'
+
+defineProps<{
+  image: string
+  alt: string
+  title: string
+  time: number
+  diet: string
+  difficulty: string
+  meal: string
+  flavour: string
+  link: string
+}>()
+</script>
+
+<style lang="scss">
+@use '@/assets/styles/variables' as *;
+.card-recipe {
+  width: 231px;
+  height: 381px;
+  box-shadow: 0px 2px 10px rgba(126, 126, 126, 0.05);
+  border-radius: 16px;
+  background-color: $white;
+  color: $black;
+  overflow: hidden;
+  position: relative;
+
+  &__button {
+    display: flex;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    background-color: black;
+    border-radius: 999px;
+    padding: 8px;
+    border: none;
+    cursor: pointer;
+  }
+
+  &__image {
+    width: 100%;
+    height: 60%;
+    object-fit: cover;
+    border-radius: 8px 8px 0 0;
+    transition: transform 0.3s ease-in-out;
+
+    &:hover {
+      cursor: pointer;
+      transform: scale(1.02);
+    }
+  }
+
+  &__info {
+    padding: 10px 16px;
+    font-size: 14px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  &__tags {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+  }
+
+  &__time {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    color: #676767;
+  }
+
+  &__title {
+    font-weight: 400;
+    font-size: 15px;
+  }
+
+  &__details {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 2px 5px;
+    position: absolute;
+    bottom: 12px;
+    width: 87%;
+    left: 10px;
+  }
+
+  &__difficulty {
+    color: $black;
+    font-size: 12px;
+    text-align: center;
+    display: flex;
+    align-items: center;
+  }
+
+  &__meal,
+  &__diet,
+  &__flavour {
+    color: #656565;
+    border: 1px solid #ffc993;
+    border-radius: 3px;
+    padding: 2px 5px;
+    font-size: 12px;
+    width: fit-content;
+  }
+}
+</style>
